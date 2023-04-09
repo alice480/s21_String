57665315fcd35029c8029731befda19ac19a9c29
Alice <sinyavina.alisa2003@mail.ru>
2023-04-09 17:03:58 +0300

    strcmp and strncpy

--- string.c
+++ string.c
@@ -1,12 +1,9 @@
 #include "string.h"
 
 int main() {
-  char a[30] = "abcfchqofkwqof ok";
-  // char* b = "cde";
-  char sym = 'c';
-  char *ptr = strchr(a, sym);
-  printf("%s", ptr);
-  // printf("%s", a);
+  char str1[1024]= "12365";
+  char str2[1024]= "i";
+  printf("%s", s21_strpbrk(str1, str2));
 }
 
 // change to s21_strlen
@@ -34,3 +31,22 @@ char *s21_strchr(const char *str, int c) {
   };
   return (char *)str + i;
 }
+
+// change to max if it is defined in stdlib.h
+int s21_strcmp(const char *str1, const char *str2) {\
+  size_t end;
+  end = (strlen(str1) < strlen(str2)) ? strlen(str2) : strlen(str1);
+  size_t i;
+  for (i = 0; i < end && str1[i] == str2[i]; i++) {};
+  return str1[i] - str2[i];
+}
+
+int s21_strncmp(const char *str1, const char *str2, size_t n) {
+  size_t end;
+  end = (strlen(str1) < strlen(str2)) ? strlen(str2) : strlen(str1);
+  size_t i;
+  for (i = 0; i < end && str1[i] == str2[i] && i < n; i++) {};
+  return str1[i] - str2[i];
+}
+
+
